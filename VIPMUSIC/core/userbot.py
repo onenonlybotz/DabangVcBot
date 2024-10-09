a7cd0c94f1f944d8650e6e5531009cd5dad8fdc1
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2024-10-09 13:40:51 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -65,10 +65,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
+                await self.one.join_chat("dabangthebrand")
             except:
                 pass
             assistants.append(1)
